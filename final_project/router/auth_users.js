9a112926cde0b56c0538318d849885b2306d497f
wolfrit <wolfrit.company@gmail.com>
2023-02-04 16:41:20 -0500

    first part

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,24 +5,78 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username) => { //returns boolean
+  //write code to check is the username is valid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = async (username, password) => { //returns boolean
+  let validusers = users.filter((user) => user.username === username && user.password === password)
+  if (validusers.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(404).json({
+      msg: "Error logging in"
+    });
+  }
+
+  if (await authenticatedUser(username, password)) {
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60 * 10 });
+
+    req.session.authorization = {
+      accessToken, username
+    }
+    return res.status(200).send({
+      msg: `User successfully logged in`
+    });
+  } else {
+    return res.status(208).json({
+      msg: "Invalid Login. Check username and password"
+    });
+  }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const {isbn} = req.params
+  const {username, stars, comment} = req.body
+
+  reviewsObject = books[Number(isbn)]['reviews']//iniciamente {}
+  if (reviewsObject[username]) {
+    books[Number(isbn)]['reviews'][username] = {stars, comment}
+  }else{
+    books[Number(isbn)]['reviews'][username] = {stars, comment}
+  }
+
+  res.status(200).send({
+    data: JSON.parse(JSON.stringify(books))
+  })  
+});
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  //Write your code here
+  const {isbn} = req.params
+  const {username} = req.body
+
+  reviewsObject = books[Number(isbn)]['reviews']//iniciamente {}
+  if (reviewsObject[username]) {
+    delete books[Number(isbn)]['reviews'][username]
+  }
+
+  res.status(200).send({
+    data: JSON.parse(JSON.stringify(books))
+  })  
 });
 
 module.exports.authenticated = regd_users;
